4d44cb989fdeb3d82e0c489195187f10ac186154
Travis CI User <travis@example.org>
2019-12-21 22:35:32 +0000

    Documentation for netvl/shellexpand@448c430

---
+++ aliases.js
@@ -0,0 +1,4 @@
+var ALIASES = {};
+ALIASES["dirs"] = {};
+ALIASES["libc"] = {};
+ALIASES["shellexpand"] = {};
---
+++ source-files.js
@@ -0,0 +1,5 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["dirs"] = {"name":"","files":["lib.rs","lin.rs","unix.rs"]};
+sourcesIndex["libc"] = {"name":"","dirs":[{"name":"unix","dirs":[{"name":"linux_like","dirs":[{"name":"linux","dirs":[{"name":"gnu","dirs":[{"name":"b64","dirs":[{"name":"x86_64","files":["align.rs","mod.rs","not_x32.rs"]}],"files":["mod.rs"]}],"files":["align.rs","mod.rs"]}],"files":["align.rs","mod.rs"]}],"files":["mod.rs"]}],"files":["align.rs","mod.rs"]}],"files":["fixed_width_ints.rs","lib.rs","macros.rs"]};
+sourcesIndex["shellexpand"] = {"name":"","files":["lib.rs"]};
+createSourceSidebar();
